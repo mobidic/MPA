5f23e5330d62588917ed31d6a0dc026c0ca653aa
Char-Al <charles.van.goethem@gmail.com>
2021-03-22 18:07:17 +0100

    Add option to enable/disable progress bar

--- mobidic_mpa/__init__.py
+++ mobidic_mpa/__init__.py
@@ -386,9 +386,13 @@ def main(args, logger):
     with open(args.input, 'r') as f:
         log.info("Read VCF file")
         vcf_reader = vcf.Reader(f)
-        count = sum(1 for _ in vcf_reader)
-        log.info(f"Number of variants : {count}")
-        f.seek(0)
+        count = -1
+        if not args.no_progress_bar:
+            count = sum(1 for _ in vcf_reader)
+            log.info(f"Number of variants : {count}")
+            f.seek(0)
+            vcf_reader = vcf.Reader(f)
+
         # TODO: improve this
         vcf_reader.infos.update({'MPA_adjusted': info_MPA_adjusted})
         vcf_reader.infos.update({'MPA_available': info_MPA_available})
@@ -406,7 +410,6 @@ def main(args, logger):
             sys.exit(1)
 
         log.info("Read each variants")
-        vcf_reader = vcf.Reader(f)
         for record in tqdm.tqdm(vcf_reader, total=count):
             log.debug(str(record))
 
